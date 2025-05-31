704b3207c6c7b6c8521a1fd787718803995acd3b
Sourav <souravmallick900@gmail.com>
2025-05-31 22:22:05 +0530

    Rendering

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const { faker } = require("@faker-js/faker");
 const mysql = require("mysql2");
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -19,32 +23,34 @@ let getRandomUser = () => {
   ];
 };
 
-app.get("/", (req, res) => {
-  let q = `SELECT count(*) FROM user`;
-  connection.query(q, (err, results) => {
-    if (err) {
-      console.log(err);
-      return res.send("Some error in database");
-    }
-    res.send("success ");
-    console.log(results[0]["count(*)"]);
-  });
-});
-
 // app.get("/", (req, res) => {
 //   let q = `SELECT count(*) FROM user`;
-//   try {
-//     connection.query(q ,(err, results) => {
-//       if (err) throw err;
-//       res.send("success");
-//       console.log(results[0]["count(*)"]);
+//   connection.query(q, (err, results) => {
+//     if (err) {
+//       console.log(err);
+//       return res.send("Some error in database");
+//     }
+//     res.send("success ");
+//     console.log(results[0]["count(*)"]);
 
-//     });
-//   } catch (err) {
-//     console.log(err);
-//     res.send("Some error in database");
-//   }
+//   });
 // });
+
+app.get("/", (req, res) => {
+  let q = `SELECT count(*) FROM user`;
+  try {
+    connection.query(q ,(err, results) => {
+      if (err) throw err;
+      let count=results[0]["count(*)"];
+    //   console.log(results[0]["count(*)"]);
+      res.render("home.ejs",{count});
+
+    });
+  } catch (err) {
+    console.log(err);
+    res.send("Some error in database");
+  }
+});
 
 app.listen(8080, () => {
   console.log("App listening on port number 8080");
